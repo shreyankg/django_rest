45f2047b37c076ca087ef557a00f8c9b224a3fed
Shreyank Gupta <shreyankg@gmail.com>
2011-09-16 01:49:03 +0530

    create, delete

--- series/urls.py
+++ series/urls.py
@@ -4,6 +4,8 @@ from views import *
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
     url(r'^(?P<soap_id>\d+)/$', show, name="show"),
+    url(r'^(?P<soap_id>\d+)/$', show, name="create"),
+    url(r'^(?P<soap_id>\d+)/$', show, name="delete"),
     url(r'^new/$', new, name="new"),
     url(r'^(?P<soap_id>\d+)/edit/$', edit, name="edit"),
 )
